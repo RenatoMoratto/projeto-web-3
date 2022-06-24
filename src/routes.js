3af6e61b6ed125f1da7d12e716a282701312a8c6
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-24 11:02:00 -0300

    feat: add private routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { postUser, authenticateUser, findAllUsers, findUserById } from "./controllers/user.js";
 import { postQuote, findAllQuotes, findQuoteById, findRandomQuote } from "./controllers/quote.js";
+import { verifyToken } from "./utils/documentoUtils.js";
 
 const router = express.Router();
 
 // User Endpoints
-router.post("/register", postUser);
+router.post("/register", verifyToken, postUser);
 router.post("/login", authenticateUser);
-router.get("/users", findAllUsers);
-router.get("/user/:id", findUserById);
+router.get("/users", verifyToken, findAllUsers);
+router.get("/user/:id", verifyToken, findUserById);
 
 // Quotes Endpoints
-router.post("/quote", postQuote);
+router.post("/quote", verifyToken, postQuote);
 router.get("/quotes", findAllQuotes);
 router.get("/quote/:id", findQuoteById);
 router.get("/quotes/random", findRandomQuote);
