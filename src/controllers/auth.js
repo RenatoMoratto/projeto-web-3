e128b6fbdebdfdc3b8e98921d1342d725c4d37e2
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-24 08:41:58 -0300

    feat: login with jwt response

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,7 @@
 const authRouter = require("express").Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { jwt_secret } = require("../environment_vars");
 const { isEmpty } = require("../utils/utils");
 const User = require("../models/User");
 
@@ -16,9 +18,6 @@ authRouter.post("/register", async (req, res) => {
 		return res.status(422).send({ message: "Password is required!" });
 	}
 
-	const hashPassword = await bcrypt.hash(password, 10);
-	const user = { name, email, password: hashPassword };
-
 	try {
 		const userExists = await User.findOne({ email: email });
 
@@ -26,8 +25,42 @@ authRouter.post("/register", async (req, res) => {
 			return res.status(400).send("Email already in use!");
 		}
 
+		const hashPassword = await bcrypt.hash(password, 10);
+		const user = { name, email, password: hashPassword };
+
 		await User.create(user);
-		res.status(200).json({ message: "User register with success!" });
+		res.status(201).json({ message: "User register with success!" });
+	} catch (error) {
+		res.status(500).json({ erro: error });
+	}
+});
+
+authRouter.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+
+	if (isEmpty(email)) {
+		return res.status(422).send({ message: "Name is required!" });
+	}
+	if (isEmpty(password)) {
+		return res.status(422).send({ message: "Email is required!" });
+	}
+
+	try {
+		const user = await User.findOne({ email: email });
+
+		if (!user) {
+			return res.status(404).send("User not found!");
+		}
+
+		const validatePassword = await bcrypt.compare(password, user.password);
+
+		if (!validatePassword) {
+			return res.status(401).send("Email and/or password are incorrect!");
+		}
+
+		const token = await jwt.sign({ id: user._id }, jwt_secret);
+
+		res.status(200).send({ access_token: token });
 	} catch (error) {
 		res.status(500).json({ erro: error });
 	}
--- src/environment_vars.js
+++ src/environment_vars.js
@@ -1,6 +1,7 @@
-const { DB_CONNECTION_STRING, PORT } = process.env;
+const { DB_CONNECTION_STRING, PORT, JWT_SECRET } = process.env;
 
 module.exports = {
 	db_connection_string: DB_CONNECTION_STRING,
 	port: PORT,
+	jwt_secret: JWT_SECRET,
 };
