4ba92a5fb749673ab0d2fbafd23a3892c53e7005
RenatoMoratto <renatowmoratto@gmail.com>
2022-06-24 15:44:16 -0300

    feat: add cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { db_connection_string, port } from "./src/environment_vars.js";
 import router from "./src/routes.js";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
